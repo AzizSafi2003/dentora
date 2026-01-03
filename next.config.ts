81f76c361b2109291fa313dda32749644868d854
Abdul Aziz <aziz20031110@gmail.com>
2026-01-03 23:39:54 +0430

    landing page completed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here if we are using images from the website directly */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

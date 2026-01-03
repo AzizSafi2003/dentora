4da871bf19de4e7c9b778b87786b181cc586c10c
Abdul Aziz <aziz20031110@gmail.com>
2026-01-04 03:18:25 +0430

    schema.prisma file added

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+// Prisma CLI configuration
+// Prisma version: 6.16.2
+
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+});
